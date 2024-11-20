258e220203290219f5f0fcf80083961b532276ba
Mantas Jankauskas <34684969+MantasJankauskas@users.noreply.github.com>
2024-11-20 19:38:37 +0200

    added requests to requirements.txt and added response get for returning sample data

--- mantas_j_mod1_atsiskaitymas/web_crawler.py
+++ mantas_j_mod1_atsiskaitymas/web_crawler.py
@@ -1,15 +1,18 @@
+import requests
+
 def crawl(source = 'eurovaistine', timeout = 60, return_format = 'json'):
     match source:
         case 'eurovaistine':
-            return __crawl_eurovaistine()
+            return __crawl_eurovaistine(timeout)
         case 'benu':
             return __crawl_benu()
         case _:
             raise ValueError
 
 
-def __crawl_eurovaistine():
-    return 'eurovaistine'
+def __crawl_eurovaistine(response_timeout):
+    response = requests.get('https://www.eurovaistine.lt/vaistai-nereceptiniai', timeout=response_timeout)
+    return response.text
 
 def __crawl_benu():
     return 'benu'
