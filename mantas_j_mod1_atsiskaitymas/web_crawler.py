8c992c3f1257a3d377810da9c4eace422ff658fb
Mantas Jankauskas <34684969+MantasJankauskas@users.noreply.github.com>
2024-11-20 20:56:31 +0200

    added eurovaistine data parser

--- mantas_j_mod1_atsiskaitymas/web_crawler.py
+++ mantas_j_mod1_atsiskaitymas/web_crawler.py
@@ -6,7 +6,7 @@ def crawl(source: str = 'eurovaistine', timeout: int = 60, return_format: str = 
         case 'eurovaistine':
             response_data = __get_web_data_as_text('https://www.eurovaistine.lt/vaistai-nereceptiniai', timeout)
             data = __parse_eurovaistine_data(response_data)
-            return None
+            return data
         case 'benu':
             response_data = __get_web_data_as_text('https://www.benu.lt/gydymas-ir-profilaktika', timeout)
             data = __parse_benu_data(response_data)
@@ -23,8 +23,15 @@ def __get_web_data_as_text(url: str, response_timeout: int) -> HTML:
     return HTML(html_content)
 
 
-def __parse_eurovaistine_data(data: HTML):
-    return data
+def __parse_eurovaistine_data(data: HTML) -> list[dict[str, str | bool]]:
+    drugs_cards = data.xpath("//a[contains(@class, 'productCard')]")
+
+    return [{
+        'title': ''.join(drug.xpath(".//div[@class='title']/span/text()")).strip(),
+        'discounted': bool(drug.xpath(".//div[contains(@class, 'discountContainer')]//div[contains(@class, 'discount')]/text()")),
+        'price': ''.join(drug.xpath(".//div[contains(@class, 'productPrice')]/span/text()")).strip()[:-2]
+    } for drug in drugs_cards]
+
 
 def __parse_benu_data(data: HTML):
     return data
