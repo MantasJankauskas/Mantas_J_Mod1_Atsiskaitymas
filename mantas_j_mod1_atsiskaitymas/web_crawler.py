16bd50bd9aafae9c8626c9e23e09030cc4a127e4
Mantas Jankauskas <34684969+MantasJankauskas@users.noreply.github.com>
2024-11-20 19:49:52 +0200

    crawl method missing return fix

--- mantas_j_mod1_atsiskaitymas/web_crawler.py
+++ mantas_j_mod1_atsiskaitymas/web_crawler.py
@@ -5,6 +5,7 @@ def crawl(source = 'eurovaistine', timeout = 60, return_format = 'json'):
         case 'eurovaistine':
             response_data = __get_web_data_as_text('https://www.eurovaistine.lt/vaistai-nereceptiniai', timeout)
             data = __parse_eurovaistine_data(response_data)
+            return None
         case 'benu':
             response_data = __get_web_data_as_text('https://www.benu.lt/gydymas-ir-profilaktika', timeout)
             data = parse_benu_data(response_data)
