6bc6117764412530ac2b54a049dd32ad9962e4ba
Mantas Jankauskas <34684969+MantasJankauskas@users.noreply.github.com>
2024-11-20 18:44:44 +0200

    init project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mantas_j_mod1_atsiskaitymas",
+    version="0.1.0",
+    author="",
+    email="",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.9"
+)
