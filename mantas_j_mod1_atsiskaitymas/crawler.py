b605ea95253c94906145c3a2973c74c3ef26ae93
Mantas Jankauskas <34684969+MantasJankauskas@users.noreply.github.com>
2024-11-21 20:50:18 +0200

    created new class Crawler and refactored web_crawler.py

---
+++ mantas_j_mod1_atsiskaitymas/crawler.py
@@ -0,0 +1,55 @@
+from lxml.etree import HTML
+from selenium import webdriver
+
+
+class Crawl:
+    accepted_formats = {'eurovaistine', 'apotheka'}
+
+    def __init__(self, source, timeout):
+        self.source = source
+        self.timeout = timeout
+        self.__validate_format()
+
+    def get_web_data(self):
+        match self.source:
+            case 'eurovaistine':
+                response_data = self.__get_web_data_as_text('https://www.eurovaistine.lt/vaistai-nereceptiniai',self.timeout)
+                return self.__parse_eurovaistine_data(response_data)
+            case 'apotheka':
+                response_data = self.__get_web_data_as_text('https://www.apotheka.lt/prekes/nereceptiniai-vaistai', self.timeout)
+                return self.__parse_apotheka_data(response_data)
+
+    def __validate_format(self):
+        if self.source not in self.accepted_formats:
+            raise ValueError(f"Invalid source: '{self.source}'. Accepted sources are: {self.accepted_formats}")
+
+    def __get_web_data_as_text(self, url: str, response_timeout: int) -> HTML:
+        driver = webdriver.Chrome()
+        driver.get(url)
+        driver.implicitly_wait(response_timeout)
+        html_content = driver.page_source
+        return HTML(html_content)
+
+
+    def __parse_eurovaistine_data(self, data: HTML) -> list[dict[str, bool]]:
+        drugs_cards = data.xpath("//a[contains(@class, 'productCard')]")
+
+        return [{
+            'title': ''.join(drug.xpath(".//div[@class='title']/span/text()")).strip(),
+            'img_url': drug.xpath(".//div[contains(@class, 'image')]//img/@src"),
+            'discounted': bool(
+                drug.xpath(".//div[contains(@class, 'discountContainer')]//div[contains(@class, 'discount')]/text()")),
+            'price': ''.join(drug.xpath(".//div[contains(@class, 'productPrice')]/span/text()")).strip()[:-2]
+        } for drug in drugs_cards]
+
+
+    def __parse_apotheka_data(self, data: HTML) -> list[dict[str, bool]]:
+        drugs_cards = data.xpath("//div[@class='box-product']")
+
+        return [{
+            'title': ''.join(drug.xpath(".//div[@class='box-product__title']/text()")).strip(),
+            'img_url': drug.xpath(".//div[contains(@class, 'box-product__image')]//img/@src"),
+            'discounted': bool(
+                drug.xpath(".//div[contains(@class, 'discountContainer')]//div[contains(@class, 'discount')]/text()")),
+            'price': ''.join(drug.xpath(".//span[@class='product-pricing__price-number']/text()")).strip()[:-2]
+        } for drug in drugs_cards]
--- mantas_j_mod1_atsiskaitymas/web_crawler.py
+++ mantas_j_mod1_atsiskaitymas/web_crawler.py
@@ -1,48 +1,7 @@
-from lxml.etree import HTML
-from selenium import webdriver
 from mantas_j_mod1_atsiskaitymas.data_parser import DataParser
+from mantas_j_mod1_atsiskaitymas.crawler import Crawl
 
 def crawl(source: str = 'eurovaistine', timeout: int = 60, return_format: str = 'json'):
-    match source:
-        case 'eurovaistine':
-            response_data = __get_web_data_as_text('https://www.eurovaistine.lt/vaistai-nereceptiniai', timeout)
-            data = __parse_eurovaistine_data(response_data)
-            parsed_data = DataParser().return_in_format(data, return_format)
-            return parsed_data
-        case 'apotheka':
-            response_data = __get_web_data_as_text('https://www.apotheka.lt/prekes/nereceptiniai-vaistai', timeout)
-            data = __parse_apotheka_data(response_data)
-            parsed_data = DataParser().return_in_format(data, return_format)
-            return parsed_data
-        case _:
-            raise ValueError(f"Invalid source: '{source}'.")
-
-
-def __get_web_data_as_text(url: str, response_timeout: int) -> HTML:
-    driver = webdriver.Chrome()
-    driver.get(url)
-    driver.implicitly_wait(response_timeout)
-    html_content = driver.page_source
-    return HTML(html_content)
-
-
-def __parse_eurovaistine_data(data: HTML) -> list[dict[str, bool]]:
-    drugs_cards = data.xpath("//a[contains(@class, 'productCard')]")
-
-    return [{
-        'title': ''.join(drug.xpath(".//div[@class='title']/span/text()")).strip(),
-        'img_url': drug.xpath(".//div[contains(@class, 'image')]//img/@src"),
-        'discounted': bool(drug.xpath(".//div[contains(@class, 'discountContainer')]//div[contains(@class, 'discount')]/text()")),
-        'price': ''.join(drug.xpath(".//div[contains(@class, 'productPrice')]/span/text()")).strip()[:-2]
-    } for drug in drugs_cards]
-
-
-def __parse_apotheka_data(data: HTML) -> list[dict[str, bool]]:
-    drugs_cards = data.xpath("//div[@class='box-product']")
-
-    return [{
-        'title': ''.join(drug.xpath(".//div[@class='box-product__title']/text()")).strip(),
-        'img_url': drug.xpath(".//div[contains(@class, 'box-product__image')]//img/@src"),
-        'discounted': bool(drug.xpath(".//div[contains(@class, 'discountContainer')]//div[contains(@class, 'discount')]/text()")),
-        'price': ''.join(drug.xpath(".//span[@class='product-pricing__price-number']/text()")).strip()[:-2]
-    } for drug in drugs_cards]
+    data = Crawl(source, timeout).get_web_data()
+    parsed_data = DataParser().return_in_format(data, return_format)
+    return parsed_data
