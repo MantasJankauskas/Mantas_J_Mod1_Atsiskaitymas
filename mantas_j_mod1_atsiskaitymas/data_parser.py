2808e6034b2a5f749bf55962f982e7ac14523162
Mantas Jankauskas <34684969+MantasJankauskas@users.noreply.github.com>
2024-11-21 19:13:48 +0200

    added csv formater in DataParser class

--- mantas_j_mod1_atsiskaitymas/data_parser.py
+++ mantas_j_mod1_atsiskaitymas/data_parser.py
@@ -1,7 +1,9 @@
 import json
+import io
+import csv
 
 class DataParser:
-    accepted_formats = {'json'}
+    accepted_formats = {'json', 'csv'}
     data_to_parse = None
 
     def __validate_format(self, return_format):
@@ -11,10 +13,25 @@ class DataParser:
     def return_in_format(self, data: list, return_format: str = 'json'):
         self.__validate_format(return_format)
         self.data_to_parse = data
-
         match return_format:
             case 'json':
                 return self.__return_json_format()
+            case 'csv':
+                return self.__return_csv_format()
 
     def __return_json_format(self):
         return json.dumps(self.data_to_parse)
+
+    def __return_csv_format(self):
+        output = io.StringIO()
+        headers = self.data_to_parse[0].keys()
+
+        writer = csv.DictWriter(output, fieldnames=headers)
+
+        writer.writeheader()
+        writer.writerows(self.data_to_parse)
+
+        csv_data = output.getvalue()
+        output.close()
+
+        return csv_data
